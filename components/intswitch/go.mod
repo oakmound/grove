735493512d65ebbf9fd2d6f112b4d9311ba87104
Patrick Stephen <patrick.d.stephen@gmail.com>
2022-01-05 19:49:18 -0600

    components/intswitch: add intswitch component

---
+++ components/intswitch/go.mod
@@ -0,0 +1,11 @@
+module github.com/oakmound/grove/components/intswitch
+
+go 1.17
+
+require github.com/oakmound/oak/v3 v3.2.1
+
+require (
+	github.com/disintegration/gift v1.2.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	golang.org/x/image v0.0.0-20201208152932-35266b937fa6 // indirect
+)
---
+++ components/intswitch/switch_test.go
@@ -0,0 +1,18 @@
+package intswitch_test 
+
+import (
+	"github.com/oakmound/grove/components/intswitch"
+	"github.com/oakmound/oak/v3/render"
+
+	"testing"
+	"image/color"
+)
+
+func TestNew(t *testing.T) {
+	sw := intswitch.New(0, map[int]render.Modifiable{
+		0: render.NewColorBox(1,1,color.RGBA{255,0,0,255}),
+	})
+	if sw == nil {
+		t.Fatal("expected non-nil switch")
+	}
+}
